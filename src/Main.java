297618e44b405cb49753bc4a10404afe59de42d4
Gareth Munnings <garethmunnings@gmail.com>
2025-02-24 13:48:44 +0200

    implemented add method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         Polynomial f = new Polynomial();
-        f.setCoefficient(2, 3);
-        f.setCoefficient(1, -2);
-        f.setCoefficient(0, 10);
+
+        f.setCoefficient(1, 2);
+        f.setCoefficient(0, 5);
+        System.out.print("f(x) = ");
         f.print();
+
+        Polynomial g = new Polynomial();
+        g.setCoefficient(2, 3);
+        g.setCoefficient(1, 1);
+        g.setCoefficient(0, -2);
+        System.out.print("g(x) = ");
+        g.print();
+
+        Polynomial h = new Polynomial();
+        h = f.add(2, g);
+
+        System.out.print("h(x) = ");
+        h.print();
         System.out.println();
-        System.out.println(f.evaluate(2));
+        //System.out.println(f.evaluate(2));
 
 
     }
--- src/Polynomial.java
+++ src/Polynomial.java
@@ -2,7 +2,7 @@ import java.util.Hashtable;
 
 public class Polynomial {
     Hashtable<Integer, Integer> terms = new Hashtable<>();
-    int highestTerm = 0;
+    public int highestTerm = 0;
 
     public void setCoefficient(int exp, int coefficient) {
         if(exp > highestTerm) {
@@ -14,7 +14,10 @@ public class Polynomial {
     }
 
     public int getCoefficient(int exp) {
-        return terms.get(exp);
+        if(terms.get(exp) != null) {
+            return terms.get(exp);
+        }
+        else return -99999;
     }
 
     public void print() {
@@ -44,5 +47,34 @@ public class Polynomial {
         }
 
         return y;
+    }
+
+    public Polynomial add(int k, Polynomial g) {
+        //scalar multiplication on g
+        for(int i = 0; i <= g.highestTerm; i++){
+            if(g.getCoefficient(i) != -99999) {
+                g.setCoefficient(i, k*g.getCoefficient(i));
+            }
+        }
+
+        //add the two polynomials
+        Polynomial h = new Polynomial();
+        for(int i = 0; i <= g.highestTerm + highestTerm; i++) {
+            if(terms.get(i) != null) {
+                if(g.getCoefficient(i) != -99999) {
+                    h.setCoefficient(i, g.getCoefficient(i) + terms.get(i));
+                }
+                else{
+                    h.setCoefficient(i, terms.get(i));
+                }
+            }
+            else{
+                if(g.getCoefficient(i) != -99999) {
+                    h.setCoefficient(i, g.getCoefficient(i));
+                }
+            }
+
+        }
+        return h;
     }
 }
